7eb6ddd1c10fe19d7e252b87238dc43d95105301
lingxiang.wang <lingxiang.wang@autonavi.com>
2014-12-31 15:02:03 +0800

    点击查看评论

--- js/post.js
+++ js/post.js
@@ -21,6 +21,15 @@ $(document).ready(function(){
         }
     };
 
+
+	window.disqus_shortname = 'fifa0329'; // required: replace example with your forum shortname
+    $('#disqus_container .comment').on('click',function(){
+        $(this).html('加载中...');
+        var that = this;
+        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
+    });
+	
+		
     $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
 
     $('.entry a').each(function(index,element){
